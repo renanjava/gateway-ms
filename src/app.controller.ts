c4e7ec1a7e15055fb058953b67c6ede198767644
renanjava <renancvr777@gmail.com>
2025-06-09 21:17:31 -0300

    update Kafka integration and dependencies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Controller, Get, OnModuleInit, Param } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+@Controller('gateway')
+export class AppController implements OnModuleInit {
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'book',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'book-consumer',
+        allowAutoTopicCreation: true,
+      },
+    },
+  })
+  private client: ClientKafka;
+  async onModuleInit() {
+    const requestPatterns = ['find-book'];
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    requestPatterns.forEach(async (pattern) => {
+      this.client.subscribeToResponseOf(pattern);
+      await this.client.connect();
+    });
+  }
+  @Get('book/:id')
+  findBook(@Param('id') id: string): Observable<any> {
+    return this.client.send('find-book', { id });
   }
 }
